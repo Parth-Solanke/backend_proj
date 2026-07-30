bb011a2172772d173ce495143fcbfc62ff95fbe1
Parth Solanke <parthrajsolanke@gmail.com>
2026-07-30 23:38:07 +0530

    mongodb connection

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// require('dotenv').config({path:'./env'})
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+
+/*
+import connectDB from "./db/index.js";
+
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+});
+
+connectDB()
+*/
+
+
+
+
+import express from "express";
+const app = express();
+
+{
+    async () => {
+        try {
+            await mongoose.connect(`${process.env.MONGODB_URI}`)
+            app.on("error", () => {
+                console.log("ERROR:", error);
+                throw error
+            })
+
+            app.listen(process.env.PORT,() => {
+                console.log(`App is listining on port ${process.env.PORT}`);
+            })
+        } catch (error) {
+            console.error("ERROR:", error)
+            throw err
+        }
+    }
+} { }
