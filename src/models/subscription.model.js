18c2937741c21c8d89aae40aff970bc2ea0f9bed
Parth Solanke <parthrajsolanke@gmail.com>
2026-08-12 21:49:03 +0530

    add watch history endpoints

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose"
 
-const subsciptionSchema = new Schema({
+const subscriptionSchema = new Schema({
     subscriber : {
         type : Schema.Types.ObjectId,
         ref : "User"
@@ -11,4 +11,4 @@ const subsciptionSchema = new Schema({
     }
 },{timestamps : true})
 
-export const Subscription = new mongoose.model("Subscription",subsciptionSchema)
+export const Subscription = new mongoose.model("Subscription",subscriptionSchema)
